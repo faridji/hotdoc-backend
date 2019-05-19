10885a0e51232342a28b8ed3d0c8013e28c681a1
Farid ullah <farid.bsse2344@iiu.edu.pk>
2019-05-19 06:42:21 -0700

    added env variable for db

--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
-
+const config = require('config');
 
 module.exports = function()
 {
     // Connection to mongodb
+    const db = config.get('db');
+    
     mongoose.set('useCreateIndex', true);
-    mongoose.connect('mongodb://localhost/hotdoc', {useNewUrlParser: true})
+    mongoose.connect(db, {useNewUrlParser: true})
         .then( () => console.log('Successfully connected to mongodb...'))
         .catch( (error) => console.log('Opps:', error.message));
 }
